ffeeabfc559102aa4ca50f975c5619a9db61772e
wangc <1091803778@qq.com>
2023-06-11 17:12:30 +0800

    tb update

--- phase2/tb/tb.v
+++ phase2/tb/tb.v
@@ -3,6 +3,15 @@ module tb;
     reg clk;
     reg rst;
 
+    // 寄存器x3：表示我们第几个test
+    // 寄存器x26：表示我们测试结束 x26 =1
+    // 寄存器x27：为0时表示fail，为1时表示pass
+    wire x3 = tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[3];
+    wire x26 = tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[26];
+    wire x27 = tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[27];
+
+
+
     always #10 clk = ~clk;
 
     initial begin
@@ -15,17 +24,27 @@ module tb;
 
     // rom initial
     initial begin
-        $readmemb("inst_data_ADD.txt", tb.open_risc_v_soc_inst.rom_inst.rom_mem);
+        $readmemb(".\inst_txt\rv32ui-p-add.txt", tb.open_risc_v_soc_inst.rom_inst.rom_mem);
     end
 
     initial begin
-        while (1) begin
-            @(posedge clk)
-            $display("-----------------------");
-            $display("x27 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[27]);
-            $display("x28 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[28]);
-            $display("x29 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[29]);
-            $display("-----------------------");
+        // while (1) begin
+        //     @(posedge clk)
+        //     $display("-----------------------");
+        //     $display("x27 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[27]);
+        //     $display("x28 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[28]);
+        //     $display("x29 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[29]);
+        //     $display("-----------------------");
+        // end
+        wait(x26 == 32'b1); // 当 x26 = 1 时程序执行完毕
+        if(x27 == 32'b1) begin
+            $display("###########     pass !     ###########");
+        end
+        else begin
+            $display("###########     fail !     ###########");
+            for (r = 0; r < 31; r = r + 1) begin
+                $display("x %2d register value is %d", r, tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[r]);
+            end
         end
     end
 
@@ -34,7 +53,7 @@ module tb;
 
     open_risc_v_soc open_risc_v_soc_inst(
         .clk        (clk),
-        .rst      (rst)
+        .rst        (rst)
     );
 
 endmodule
