987386fd0f47b4a352bb9926306951c281d392b8
wangc <1091803778@qq.com>
2023-06-12 16:31:38 +0800

    i

--- phase2/rtl/ex.v
+++ phase2/rtl/ex.v
@@ -26,6 +26,7 @@ module ex(
 	wire[4:0] rs2;
 	wire[6:0] func7;
 	wire[11:0]imm;
+	wire[4:0] shamt;
 	
 	assign opcode = inst_i[6:0];
 	assign rd 	  = inst_i[11:7];
@@ -34,6 +35,7 @@ module ex(
 	assign rs2 	  = inst_i[24:20];
 	assign func7  = inst_i[31:25];
 	assign imm    = inst_i[31:20];
+	assign shamt  = inst_i[24:20];
 	
 	
 	
@@ -41,11 +43,15 @@ module ex(
 	wire[31:0] jump_imm = {{19{inst_i[31]}},inst_i[31],inst_i[7],inst_i[30:25],inst_i[11:8],1'b0}; //改 21+6+4+1
 	wire  	   op1_i_equal_op2_i;
 	wire 	   op1_i_less_op2_i_signed;
+	wire 	   op1_i_less_op2_i_unsigned;
 	
-	assign	   op1_i_equal_op2_i       = (op1_i == op2_i)?1'b1:1'b0;
-	assign	   op1_i_less_op2_i_signed = ($signed(op1_i) < $signed(op2_i))?1'b1:1'b0;
+	assign	   op1_i_equal_op2_i         = (op1_i == op2_i)?1'b1:1'b0;
+	assign	   op1_i_less_op2_i_signed   = ($signed(op1_i) < $signed(op2_i))?1'b1:1'b0;
+	assign	   op1_i_less_op2_i_unsigned = (op1_i) < (op2_i)?1'b1:1'b0;
 
 	// Type I
+	wire[31:0] SRA_mask;
+	assign 	   SRA_mask = (32'hffff_ffff) >> op2_i[4:0];
 
 	
 	always @(*)begin
@@ -63,49 +69,55 @@ module ex(
 					end
 
 					`INST_SLTI:begin
-						rd_data_o = op1_i + op2_i;
+						rd_data_o = {30'b0, op1_i_less_op2_i_signed};
 						rd_addr_o = rd_addr_i;
 						rd_wen_o  = 1'b1;
 					end
 
 					`INST_SLTIU:begin
-						rd_data_o = op1_i + op2_i;
+						rd_data_o = {30'b0, op1_i_less_op2_i_unsigned};
 						rd_addr_o = rd_addr_i;
 						rd_wen_o  = 1'b1;
 					end
 
 					`INST_XORI:begin
-						rd_data_o = op1_i + op2_i;
+						rd_data_o = op1_i ^ op2_i;
 						rd_addr_o = rd_addr_i;
 						rd_wen_o  = 1'b1;
 					end
 
 					`INST_ORI:begin
-						rd_data_o = op1_i + op2_i;
+						rd_data_o = op1_i | op2_i;
 						rd_addr_o = rd_addr_i;
 						rd_wen_o  = 1'b1;
 					end
 
 					`INST_ADNI:begin
-						rd_data_o = op1_i + op2_i;
+						rd_data_o = op1_i & op2_i;
 						rd_addr_o = rd_addr_i;
 						rd_wen_o  = 1'b1;
 					end
 
 					`INST_SLLI:begin
-						rd_data_o = op1_i + op2_i;
+						rd_data_o = op1_i << shamt;
 						rd_addr_o = rd_addr_i;
 						rd_wen_o  = 1'b1;
 					end
 
 					`INST_SRI:begin
 						if (func7[5] == 1'b1) begin // SRAI
-							
+							rd_data_o = ((op1_i >> shamt) & SRA_mask) | ({32{op1_i[31]}} & (~SRA_mask));
+						    rd_addr_o = rd_addr_i;
+						    rd_wen_o  = 1'b1;
 						end
+
 						else begin   // SRLI
-						  
+						   rd_data_o = op1_i >> shamt;
+						   rd_addr_o = rd_addr_i;
+						   rd_wen_o  = 1'b1;
 						end
 					end
+
 					default:begin
 						rd_data_o = 32'b0;
 						rd_addr_o = 5'b0;
