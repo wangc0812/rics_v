ee685de6a40c98c34dbf9be052936cdf60d8a216
wangc <1091803778@qq.com>
2023-06-12 15:21:33 +0800

    regs

--- phase2/rtl/regs.v
+++ phase2/rtl/regs.v
@@ -20,34 +20,34 @@ module regs(
 
 	always @(*)begin
 		if(rst == 1'b0)
-			reg1_rdata_o <= 32'b0;
+			reg1_rdata_o = 32'b0;
 		else if(reg1_raddr_i == 5'b0)
-			reg1_rdata_o <= 32'b0;
+			reg1_rdata_o = 32'b0;
 		else if(reg_wen && reg1_raddr_i == reg_waddr_i)
-			reg1_rdata_o <= reg_wdata_i;
+			reg1_rdata_o = reg_wdata_i;
 		else
-			reg1_rdata_o <= regs[reg1_raddr_i];
+			reg1_rdata_o = regs[reg1_raddr_i];
 	end
 	
 	always @(*)begin
 		if(rst == 1'b0)
-			reg2_rdata_o <= 32'b0;
+			reg2_rdata_o = 32'b0;
 		else if(reg2_raddr_i == 5'b0)
-			reg2_rdata_o <= 32'b0;
+			reg2_rdata_o = 32'b0;
 		else if(reg_wen && reg2_raddr_i == reg_waddr_i)
-			reg2_rdata_o <= reg_wdata_i;
+			reg2_rdata_o = reg_wdata_i;
 		else
-			reg2_rdata_o <= regs[reg2_raddr_i];
+			reg2_rdata_o = regs[reg2_raddr_i];
 	end
 
 	always @(posedge clk)begin
 		if(rst == 1'b0) begin
-			for(i=0;i<31;i=i+1)begin
-				regs[i] <= 32'b0;
+			for(i = 0;i < 31;i = i + 1)begin
+				regs[i] = 32'b0;
 			end
 		end	
 		else if(reg_wen && reg_waddr_i != 5'b0)begin
-			regs[reg_waddr_i] <= reg_wdata_i;
+			regs[reg_waddr_i] = reg_wdata_i;
 		end	
 	end
 
